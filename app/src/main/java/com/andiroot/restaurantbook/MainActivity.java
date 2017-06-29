5804f2ed5dd49f6fa143f77a4010a4a9315d93c4
Imshyamal <shyamalkapatel@gmail.com>
2017-06-29 12:00:36 -0400

    Added imageview with back button

--- app/src/main/java/com/andiroot/restaurantbook/MainActivity.java
+++ app/src/main/java/com/andiroot/restaurantbook/MainActivity.java
@@ -10,29 +10,53 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private ArrayList<String> mSelecetdItems = new ArrayList<>();
+
+    public static final String SELECETD_ITEMS = "SELECETD_ITEMS";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-
     }
+
+
     public  void addtable(View v){
-        Intent i = new Intent(this,Second.class);
-        i.putExtra("table",1);
-        startActivity(i);
+        this.mSelecetdItems.add("table");
+        showSecondActivity();
     }
     public  void addchair(View v){
+        this.mSelecetdItems.add("chair");
+        showSecondActivity();
+    }
+
+
+    private void showSecondActivity()
+    {
         Intent i = new Intent(this,Second.class);
-        i.putExtra("chair",1);
+        i.putStringArrayListExtra(SELECETD_ITEMS,this.mSelecetdItems);
         startActivity(i);
     }
 
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putStringArrayList(SELECETD_ITEMS , mSelecetdItems);
     }
 
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        this.mSelecetdItems = savedInstanceState.getStringArrayList(SELECETD_ITEMS);
+    }
+
+
+}
--- app/src/main/java/com/andiroot/restaurantbook/Second.java
+++ app/src/main/java/com/andiroot/restaurantbook/Second.java
@@ -5,19 +5,19 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
 import android.support.v7.widget.LinearLayoutCompat;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class Second extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.second_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.second_toolbar);
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
@@ -37,28 +37,38 @@ public class Second extends AppCompatActivity {
 
         LinearLayoutCompat hall = (LinearLayoutCompat) findViewById(R.id.hall_layout);
         Intent i = getIntent();
-        int x = i.getIntExtra("table",0);
-        int y = i.getIntExtra("chair",0);
-        if (x==1) {
-            ImageView iv = new ImageView(this);
-            iv.setImageResource(R.drawable.table);
-            iv.setLayoutParams(new android.view.ViewGroup.LayoutParams(200,200));
-            iv.setMaxHeight(120);
-            iv.setMaxWidth(120);
-            hall.addView(iv);
+
+
+        ArrayList<String> selecetdItems = i.getStringArrayListExtra(MainActivity.SELECETD_ITEMS);
+        for (String selecetdItem : selecetdItems) {
+            if (selecetdItem.equals("table")) {
+                addImageView(R.drawable.table, hall);
+            } else if (selecetdItem.equals("chair")) {
+                addImageView2(R.drawable.chair2, hall);
+            }
         }
-        if (y==1) {
-            ImageView iv = new ImageView(this);
-            iv.setImageResource(R.drawable.chair);
-            iv.setLayoutParams(new android.view.ViewGroup.LayoutParams(200,200));
-            iv.setMaxHeight(120);
-            iv.setMaxWidth(120);
-            hall.addView(iv);
-        }
+
+
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
+    private void addImageView(int imageRes, LinearLayoutCompat hall) {
+        ImageView iv = new ImageView(this);
+        iv.setImageResource(imageRes);
+        iv.setLayoutParams(new android.view.ViewGroup.LayoutParams(250, 250));
+        iv.setMaxHeight(180);
+        iv.setMaxWidth(120);
+        iv.setPadding(15,15,0,0);
+        hall.addView(iv);
     }
+
+    private void addImageView2(int imageRes, LinearLayoutCompat hall) {
+        ImageView iv2 = new ImageView(this);
+        iv2.setImageResource(imageRes);
+        iv2.setLayoutParams(new android.view.ViewGroup.LayoutParams(150, 150));
+        iv2.setMaxHeight(30);
+        iv2.setMaxWidth(30);
+        iv2.setPadding(25,0,0,0);
+        hall.addView(iv2);
+    }
+
 }
